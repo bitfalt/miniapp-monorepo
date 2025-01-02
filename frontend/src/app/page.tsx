c748a9aad898545f30aad11c65f7ded1877c3c7f
evgongora <evasquezgongora@gmail.com>
2025-01-02 14:14:05 -0600

    feat: add sidebar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import { BannerTop } from '@/components/ui/BannerTop'
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <BannerTop />
-    </div>
-  )
+  return null
 }
