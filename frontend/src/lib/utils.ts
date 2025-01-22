a17982c8e71e1f9c27eb6fc7f3422d1c53f4649d
bitfalt <75431447+bitfalt@users.noreply.github.com>
2025-01-22 08:02:01 -0600

    add: xata api key to xata client

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { XataClient } from "./xata"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const xata = new XataClient({ apiKey: process.env.XATA_API_KEY });
