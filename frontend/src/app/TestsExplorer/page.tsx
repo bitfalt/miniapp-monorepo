b85c4ef29496804033369d052c827618331b25bc
Hellen Xie <hellenxie09@gmail.com>
2025-01-22 21:53:23 -0600

    update: add changes to Tests Explorer screen

--- frontend/src/app/TestsExplorer/page.tsx
+++ frontend/src/app/TestsExplorer/page.tsx
@@ -37,8 +37,8 @@ export default function TestsPage() {
   }
 
   return (
-    <main className="min-h-screen bg-[#FFFFFF] p-4 sm:p-6 md:p-8">
-      <div className="w-full bg-gradient-to-br from-[#387478] to-[#2C5154] text-white p-8 sm:p-10 md:p-12 rounded-b-3xl">
+    <main className="min-h-screen bg-[#FFFFFF] p-0">
+      <div className="w-full bg-gradient-to-br from-[#387478] to-[#2C5154] text-white p-8 sm:p-10 md:p-12 rounded-b-3xl rounded-t-none">
         <div className="flex justify-center items-start mb-6 md:mb-8">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-center w-full">
             Your journey to Self-Awareness
@@ -57,7 +57,7 @@ export default function TestsPage() {
           </p>
         </div>
       </div>
-      <div className="space-y-6 max-w-md mx-auto mt-4"> 
+      <div className="space-y-6 max-w-md mx-auto mt-4 flex flex-col items-center">
         {filteredTests.map((test, index) => (
           <TestCard
             key={index}
@@ -70,7 +70,7 @@ export default function TestsPage() {
         ))}
       </div>
 
-      <p className="text-center mt-8 text-xs sm:text-sm text-gray-600 font-bold">
+      <p className="text-center mt-4 text-xs sm:text-sm text-gray-600 font-bold">
         Hey! There are more tests Coming Soon! 🎉
       </p>
     </main>
